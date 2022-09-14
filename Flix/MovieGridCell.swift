b1a99167e995fa07bffdd18c641494f90f416b32
Michelob Revol <93354693+SizomanHuxley@users.noreply.github.com>
2022-09-13 21:24:32 -0400

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  movieGridCell.swift
+//  Flix
+//
+//  Created by Michelob Revol on 9/12/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+  
+    @IBOutlet weak var posterView: UIView!
+    
+    
+}
